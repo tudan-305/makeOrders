53cbbbe13032b413525dfee5d95c89bac4aae6b1
Tancheng <yingtc@hotmail.com>
2026-04-20 17:17:57 +0800

    fix: 细微调整, 添加医疗器械数据库csv文件

--- src/ui/salemode_block.py
+++ src/ui/salemode_block.py
@@ -1,28 +1,45 @@
-from PySide6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout,
-                               QPushButton, QLabel, QLineEdit, QComboBox, 
-                               QRadioButton)
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout,QPushButton,
+                               QButtonGroup, QLabel, QLineEdit, QRadioButton)
 from PySide6.QtCore import Signal, Slot
 
 #区块2制作
 class SalemodeBlock(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.suppliers = {"喜鱼":"杭州喜鱼医疗科技有限公司",
+                          "九州通": "浙江九州通医疗器械有限公司"}
+        self.sale_modes = ['器械', '寄售', '折让', '赠送', '换货']
+        self.order_modes = ['分开制作', '合并制作']
+        # 创建三个隔离的qbuttongroup 来管理qradiobutton
+        self.suppliers_group = QButtonGroup()
+        self.salemodes_group = QButtonGroup()
+        self.ordermodes_group = QButtonGroup()
         self.set_ui()
-    
 
     def set_ui(self):
-        layout1 = QHBoxLayout()
-        
-        for item in ['器械', '寄售', '折让', '赠送', '换货']:
-            btn = QRadioButton(item)
-            layout1.addWidget(btn)
+        # 创建QRadioButton放入QButtonGroup并放入layout
+        row1 = QHBoxLayout()
+        row1.addWidget(QLabel("供应商："))
+        for num, i in enumerate(self.suppliers.keys(), 1):
+            btn = QRadioButton(i)
+            self.suppliers_group.addButton(btn, num)
+            row1.addWidget(btn)
 
-        layout2 = QHBoxLayout()
-        layout2.addWidget(QLabel("产品单、赠品单："))
-        for item in ['分开制作', '合并制作']:
-            btn = QRadioButton(item)
-            layout2.addWidget(btn)
+        row2 = QHBoxLayout()
+        for i in self.sale_modes:
+            btn = QRadioButton(i)
+            self.salemodes_group.addButton(btn)
+            row2.addWidget(btn)
+
+        row3 = QHBoxLayout()
+        row3.addWidget(QLabel("赠品单："))
+        for i in self.order_modes:
+            btn = QRadioButton(i)
+            self.ordermodes_group.addButton(btn)
+            row3.addWidget(btn)
+        
         
         layout = QVBoxLayout(self)
-        layout.addLayout(layout1)
-        layout.addLayout(layout2)
+        layout.addLayout(row1)
+        layout.addLayout(row2)
+        layout.addLayout(row3)
